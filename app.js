a72d85ffc2ff499ee82aedc5c02bcf3b6199f41c
Quinton Fults <quinton.fults.work@gmail.com>
2020-08-10 14:02:19 -0700

    code

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+// fs.readFile('example.txt', 'utf8', (err, data) => {
+//   if (err) { console.log(err) }
+//   console.log(data)
+// })
+
+// fs.writeFile('something.txt', 'Hi', (err) => {
+//   if (err) { console.log(err) }
+//   console.log('File Created!')
+// })
+
+// fs.appendFile('example.txt', ' Goodbye World!', (err) => {
+//   if (err) { console.log(err) }
+//   console.log('File Updated!')
+// })
+
